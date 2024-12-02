35d2b5d6bfd6d626f3c208c1b89865a0e68b1da2
teperie <77098015+teperie@users.noreply.github.com>
2024-12-03 03:05:01 +0900

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,11 @@
 FROM steamcmd/steamcmd:latest
 
-WORKDIR /dst
+RUN apt-get update && \
+    apt-get install -y libcurl4-gnutls-dev:i386 &&\
+    apt-get autoremove --purge -y wget && \
+    apt-get clean && rm -rf /var/lib/apt/lists/*
+
+WORKDIR /root
 
 RUN mkdir -p .klei/DoNotStarveTogether server_dst/mods
 
@@ -9,11 +14,11 @@ RUN steamcmd \
     +@ShutdownOnFailedCommand 1 \
     +@NoPromptForPassword 1 \
     +login anonymous \
-    +force_install_dir /dst/server_dst \
+    +force_install_dir /root/server_dst \
     +app_update 343050 validate \
     +quit
 
-VOLUME ["/dst/.klei/DoNotStarveTogether", "/dst/server_dst/mods"]
+VOLUME ["/root/.klei/DoNotStarveTogether", "/root/server_dst/mods"]
 
-COPY ["start-container-server.sh", "/dst/"]
-ENTRYPOINT ["bash", "/dst/start-container-server.sh"]
+COPY ["start-container-server.sh", "/root/"]
+ENTRYPOINT ["bash", "/root/start-container-server.sh"]
--- start-container-server.sh
+++ start-container-server.sh
@@ -2,22 +2,22 @@
 
 # Check for game updates before each start. If the game client updates and your server is out of date, you won't be
 # able to see it on the server list. If that happens just restart the containers and you should get the latest version
-steamcmd +@ShutdownOnFailedCommand 1 +@NoPromptForPassword 1 +login anonymous +force_install_dir /dst/server_dst +app_update 343050 validate +quit
+steamcmd +@ShutdownOnFailedCommand 1 +@NoPromptForPassword 1 +login anonymous +force_install_dir /root/server_dst +app_update 343050 validate +quit
 
 # Copy dedicated_server_mods_setup.lua
-ds_mods_setup="/dst/.klei/DoNotStarveTogether/DSTWhalesCluster/mods/dedicated_server_mods_setup.lua"
+ds_mods_setup="$HOME/.klei/DoNotStarveTogether/DSTWhalesCluster/mods/dedicated_server_mods_setup.lua"
 if [ -f "$ds_mods_setup" ]
 then
-  cp $ds_mods_setup "/dst/server_dst/mods/"
+  cp $ds_mods_setup "$HOME/server_dst/mods/"
 fi
 
 # Copy modoverrides.lua
-modoverrides="/dst/.klei/DoNotStarveTogether/DSTWhalesCluster/mods/modoverrides.lua"
+modoverrides="$HOME/.klei/DoNotStarveTogether/DSTWhalesCluster/mods/modoverrides.lua"
 if [ -f "$modoverrides" ]
 then
-  cp $modoverrides "/dst/.klei/DoNotStarveTogether/DSTWhalesCluster/Master/"
-  cp $modoverrides "/dst/.klei/DoNotStarveTogether/DSTWhalesCluster/Caves/"
+  cp $modoverrides "$HOME/.klei/DoNotStarveTogether/DSTWhalesCluster/Master/"
+  cp $modoverrides "$HOME/.klei/DoNotStarveTogether/DSTWhalesCluster/Caves/"
 fi
 
-cd /dst/server_dst/bin
+cd $HOME/server_dst/bin
 ./dontstarve_dedicated_server_nullrenderer -cluster DSTWhalesCluster -shard "$SHARD_NAME"
